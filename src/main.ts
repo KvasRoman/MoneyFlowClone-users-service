a1410d42068500282e67ea9b1d6bfea3968f140c
KvasRoman <romankhmil0705@gmail.com>
2025-02-22 02:30:56 -0800

    init users-service

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: { host: '127.0.0.1', port: 3001 }, // Auth-service runs on port 3001
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'], // ✅ Enable all log levels
+  });
+
+  console.log('Auth Microservice is running on port 3001');
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
